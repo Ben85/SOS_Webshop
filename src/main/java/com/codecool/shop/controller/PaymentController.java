564fef02f36cec476341bf35ac2d4d0ba8a86628
Balint Bakcsa <balintbakcsa@gmail.com>
2018-04-18 17:24:35 +0200

    Add render template for payment controller

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -17,12 +17,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/"})
+@WebServlet(urlPatterns = {"/payment"})
 public class PaymentController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        // TODO: Render payment template using shopping cart from session data
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        engine.process("payment/payment.html", context, resp.getWriter());
     }
 
     @Override
