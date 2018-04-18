e95ed61980dc7af2883fcfeb50c3509848c825d2
Tamás Varjas <tamas.varjas00@gmail.com>
2018-04-18 16:15:01 +0200

    Moved up getShoppingCart method to abstract class

--- src/main/java/com/codecool/shop/controller/AbstractController.java
+++ src/main/java/com/codecool/shop/controller/AbstractController.java
@@ -8,12 +8,20 @@ import javax.servlet.http.HttpSession;
 
 public abstract class AbstractController extends HttpServlet {
 
+    private final String SHOPPING_CART_SESSION_KEY = "shoppingCart";
+    private ShoppingCart shoppingCart;
+
     void initializeShoppingCart(HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session.isNew()) {
             ShoppingCart shoppingCart = new ShoppingCart();
-            session.setAttribute(ShoppingCartController.SHOPPING_CART_SESSION_KEY, shoppingCart);
+            session.setAttribute(SHOPPING_CART_SESSION_KEY, shoppingCart);
         }
+    }
+
+    ShoppingCart getShoppingCart(HttpServletRequest request) {
+        shoppingCart = (ShoppingCart) request.getSession().getAttribute(SHOPPING_CART_SESSION_KEY);
+        return shoppingCart;
     }
 
 }
