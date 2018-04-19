1cc4981dc22bcfecd926be912694b12fa44a5332
Bende Hajdu <outdoorsport85@gmail.com>
2018-04-19 21:55:47 +0200

    front page cart design and slider animation

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -37,6 +37,7 @@ public class ProductController extends AbstractController {
         context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.find(1));
         context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        context.setVariable("shoppingCart", getShoppingCart(req).getItemList());
         engine.process("product/index.html", context, resp.getWriter());
     }
 
