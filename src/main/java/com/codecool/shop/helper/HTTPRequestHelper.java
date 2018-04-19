fd3f4024947e961ced3c86e2f5f2c98d3593d0f6
Balint Bakcsa <balintbakcsa@gmail.com>
2018-04-19 13:41:51 +0200

    Create HTTPRequest helper class

---
+++ src/main/java/com/codecool/shop/helper/HTTPRequestHelper.java
@@ -0,0 +1,91 @@
+package com.codecool.shop.helper;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+// Singleton class for managing http requests:
+public class HTTPRequestHelper {
+    private HTTPRequestHelper() {}
+
+    private static HTTPRequestHelper singletonInstance = new HTTPRequestHelper();
+
+    public static HTTPRequestHelper getInstance() {
+        return singletonInstance;
+    }
+
+    private HttpURLConnection createHTTPConnection(URL targetURL, HashMap<String, String> requestProperties)
+        throws IOException
+    {
+        HttpURLConnection connection = (HttpURLConnection) targetURL.openConnection();;
+
+        for (Map.Entry<String, String> property : requestProperties.entrySet()) {
+            connection.setRequestProperty(
+                property.getKey(),
+                property.getValue()
+            );
+        }
+
+        connection.setUseCaches(false);
+        connection.setDoOutput(true);
+
+        return connection;
+    }
+
+    private void sendRequest(OutputStream requestStream, String urlParameters)
+        throws IOException
+    {
+        DataOutputStream writer = new DataOutputStream(requestStream);
+        writer.writeBytes(urlParameters);
+        writer.close();
+    }
+
+    private String getResponse(InputStream responseStream)
+        throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(responseStream));
+        StringBuilder response = new StringBuilder();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+            response.append('\r');
+        }
+        reader.close();
+        return response.toString();
+    }
+
+    private String executeRequest(String method, String targetURL, String urlParameters)
+        throws IOException
+    {
+        URL url = new URL(targetURL);
+        HashMap<String, String> requestProperties = new HashMap<String, String>() {{
+            put("Content-Type", "application/x-www-form-urlencoded");
+            put("Content-Length", Integer.toString(urlParameters.getBytes().length));
+            put("Content-Language", "en-US");
+        }};
+
+        HttpURLConnection connection = createHTTPConnection(url, requestProperties);
+
+        sendRequest(connection.getOutputStream(), urlParameters);
+
+        return getResponse(connection.getInputStream());
+    }
+
+    public String executePost(String targetUrl, String[] urlParameters)
+        throws IOException
+    {
+        return executeRequest("POST", targetUrl, String.join(",", urlParameters));
+    }
+
+    public String executeGet(String targetUrl, String[] urlParameters)
+        throws IOException
+    {
+        return executeRequest("GET", targetUrl, String.join(",", urlParameters));
+    }
+}
