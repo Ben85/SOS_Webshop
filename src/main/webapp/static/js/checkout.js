b8d0a77a0e8d55ee050fd628f2c94fc9c064eea4
Tamás Varjas <tamas.varjas00@gmail.com>
2018-04-19 12:59:24 +0200

    Validate name and city

--- src/main/webapp/static/js/checkout.js
+++ src/main/webapp/static/js/checkout.js
@@ -6,15 +6,37 @@ dom = {
     validateData() {
         let submitBtn = document.getElementById("submit");
         submitBtn.addEventListener("click", function () {
-            // dom.validateText();
+            dom.validateName();
+            dom.validateCity();
             dom.validateEmail();
         })
     },
 
-    // validateText() {
-    //     const textRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
-    //     let name = document.getElementById("name");
-    // },
+    validateText(fieldId, errorId) {
+        const textRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-]+$/u;
+        let field = document.getElementById(fieldId);
+        let value = field.value;
+        let errorField = document.getElementById(errorId);
+        if (!textRegex.test(value)) {
+            errorField.innerText = `Please enter a valid ${fieldId}!`;
+        } else {
+            errorField.innerText = "";
+        }
+    },
+
+    validateName() {
+        let fieldId = "name";
+        let errorId = "nameError";
+        validateText(fieldId, errorId);
+    }
+    ,
+
+    validateCity() {
+        let fieldId = "city";
+        let errorId = "cityError";
+        validateText(fieldId, errorId);
+    }
+    ,
 
     validateEmail() {
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -28,6 +50,7 @@ dom = {
         }
     }
 
-};
+}
+;
 
 dom.init();
