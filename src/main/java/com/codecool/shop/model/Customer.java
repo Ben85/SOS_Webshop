8425131bc8be9b252cc11d5477614f39af789757
Tamás Varjas <tamas.varjas00@gmail.com>
2018-05-03 18:25:40 +0200

    Modify Customer class with new fields

--- src/main/java/com/codecool/shop/model/Customer.java
+++ src/main/java/com/codecool/shop/model/Customer.java
@@ -4,17 +4,20 @@ import java.util.HashMap;
 
 public class Customer {
 
-    private String name;
-    private int id;
     private static int count = 0;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String hashedPassword;
     private String email;
     private String phoneNum;
-    private String bZip;
     private String zip;
     private String city;
-    private String bCity;
     private String address;
+    private String bZip;
+    private String bCity;
     private String bAddress;
+    private String username;
     private String isSameAddress;
     HashMap<String, String> userData = new HashMap<>();
 
@@ -25,39 +28,48 @@ public class Customer {
         userData = (HashMap<String, String>) userInput.clone();
     }
 
-    public Customer(String name, String email, String phoneNum, String bZip, String zip, String city, String bCity, String address, String bAddress, String isSameAddress) {
-        this.name = name;
+    public Customer(String firstName, String lastName, String hashedPassword, String email, String phoneNum, String bZip, String zip, String city, String bCity, String address, String bAddress, String username, String isSameAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hashedPassword = hashedPassword;
         this.email = email;
         this.phoneNum = phoneNum;
-        this.bZip = bZip;
         this.zip = zip;
         this.city = city;
-        this.bCity = bCity;
         this.address = address;
+        this.bZip = bZip;
+        this.bCity = bCity;
         this.bAddress = bAddress;
+        this.username = username;
         this.isSameAddress = isSameAddress;
     }
 
-    public Customer(String name, String email, String phoneNum, String zip, String city, String address) {
-        this.name = name;
+    public Customer(String firstName, String lastName, String hashedPassword, String email, String phoneNum, String zip, String city, String address, String username) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hashedPassword = hashedPassword;
         this.email = email;
         this.phoneNum = phoneNum;
         this.zip = zip;
         this.city = city;
         this.address = address;
+        this.username = username;
     }
 
     public boolean getIsSameAddress() {
         return Boolean.valueOf(isSameAddress);
     }
 
-
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
     }
 
     public String getEmail() {
@@ -122,6 +134,10 @@ public class Customer {
 
     public void setbAddress(String bAddress) {
         this.bAddress = bAddress;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public HashMap<String, String> getUserData() {
