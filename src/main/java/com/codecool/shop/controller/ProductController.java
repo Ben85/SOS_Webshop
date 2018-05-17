d6b4ab1ac5abf3f6d3a28c0c768929e9eceed7a1
Tamas Horvath <horvathtamas22@gmail.com>
2018-05-17 15:10:36 +0200

    login works

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,11 +2,9 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoDatabase;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoDatabase;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Customer;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -14,8 +12,10 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
@@ -37,4 +37,25 @@ public class ProductController extends AbstractController {
         renderTemplate("product/index.html", req, resp, context);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        CustomerDaoDatabase customers = CustomerDaoDatabase.getInstance();
+        List<Customer> customerList = customers.getAll();
+        for (Customer customer : customerList) {
+            if(username.equals(customer.getUsername())) {
+                if(password.equals(customer.getHashedPassword())) {//Hash.isPasswordCorrect(password, customer.getHashedPassword())) {
+                    session.setAttribute("customer", customer);
+                    resp.sendRedirect("/");
+                }
+            } else {
+
+            }
+        }
+
+    }
 }
